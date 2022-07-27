4d9d780398053f3c4f5e39ff201c4803294bd626
Artem Rybakov <gizmo3000@yandex.ru>
2022-07-27 14:37:00 +0300

    moved Storage interface to domain

--- domain/user.go
+++ domain/user.go
@@ -1,6 +1,10 @@
+// package domain represents core types
+// for the authentication service.
 package domain
 
-// User - user's entity
+import "context"
+
+// User - user's entity.
 type User struct {
 	Login string `json:"login"`
 	Password
@@ -8,9 +12,17 @@ type User struct {
 	IsDisabled bool  `json:"is_disabled"`
 }
 
-// Password is the hashed password
+// Password is the hashed password.
 type Password struct {
 	Hash        string `json:"-"`
 	GeneratedAt int64  `json:"generated_at"`
 	IsActive    bool   `json:"is_active"`
+}
+
+// Storage is the contract that database must implement.
+type Storage interface {
+	AddUser(context.Context, User) error
+	User(ctx context.Context, login string) (User, error)
+	UpdatePassword(context.Context, User) error
+	Password(context.Context, User) (Password, error)
 }
